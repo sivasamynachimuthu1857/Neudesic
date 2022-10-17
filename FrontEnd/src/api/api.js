c3cf89ecabb9f2007200cac3bfc570478360ce2b
sivasamynachimuthu1857 <sivasamy.nachimuthu@neudesic.com>
2022-10-17 14:59:10 +0530

    Ci test

--- FrontEnd/src/api/api.js
+++ FrontEnd/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "https://siva-pep-neudesic-api.azurewebsites.net";
 
 class LocationApi {
   static token;
